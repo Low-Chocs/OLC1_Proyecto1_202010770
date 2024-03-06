e2c36f9bb414020b66c8167646468557a697a467
Chocs <Luisitom@gmail.com>
2024-03-06 12:05:51 -0600

    Se comenzo a trabajar en la lista

---
+++ OC1_Proyecto1_202010770-c16f29ba7758fc053508a808af85b64f088b19d9/src/proyecto1_oc1_202010770/lista.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyecto1_oc1_202010770;
+import java.util.ArrayList;
+import proyecto1_oc1_202010770.tabla_de_simbolos;
+/**
+ *
+ * @author Luisi
+ */
+public class lista {
+    public String variable = "";
+    public ArrayList<tabla_de_simbolos> lista;
+    
+    
+    public lista(String variable) {
+        this.variable = variable;
+        this.lista = new ArrayList<>();
+    }
+    
+    public void agregarElemento(String tipo, String variable, String dato) {
+        this.lista.add(new tabla_de_simbolos(tipo, variable, dato));
+    }
+
+    public void mostrarElementos() {
+        for (tabla_de_simbolos elemento : this.lista) {
+            System.out.println("Tipo: " + elemento.tipo + ", Variable: " + elemento.variable + ", Dato: " + elemento.dato);
+        }
+    }
+}
